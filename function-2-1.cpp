0b3c6f479608f5d7fb14e67cf8689f14583540ad
Parth2004 <100551639+Parth2004@users.noreply.github.com>
2022-04-01 11:51:59 +1030

    Add files via upload

--- function-2-1.cpp
+++ function-2-1.cpp
@@ -1,67 +1,22 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
+//array to store all months
+string arr[] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+
+//function to get month from index
 string lookup_month(int month){
-
-for (int i = 0; i < month; i++) {
-    cout << "Enter the Month";
-    cin >> month;
+        
+        //variable to store month
+        string str = "";
+        
+        //if month is valid return from array
+        if(month >= 1 && month <= 12)
+                str = arr[month-1];
+        //otherwise set month as invalid        
+        else
+                str = "invalid month";
+    
+        return str;
+        
 }
-
-
-{
-
-    for (int i = 0; i < month; i++) // for loop repeates the length time
-
-    {
-        switch (month) // switch case for the digits in the array called numbers
-        {
-        case 1:
-            cout << i << " " << month << " jan" << endl; // if it is 1 then pritns the 1
-            break;
-        case 2:
-            cout << i << " " << month << " Feb" << endl;
-            break;
-        case 3:
-            cout << i << " " << month << " Mar" << endl;
-            break;
-        case 4:
-            cout << i << " " << month << " Apr" << endl;
-            break;
-        case 5:
-            cout << i << " " << month << " May" << endl;
-            break;
-        case 6:
-            cout << i << " " << month << " jun" << endl;
-            break;
-        case 7:
-            cout << i << " " << month << " Jul" << endl;
-            break;
-        case 8:
-            cout << i << " " << month << " Aug" << endl;
-            break;
-        case 9:
-            cout << i << " " << month << " Sep" << endl;
-            break;
-        case 10:
-            cout << i << " " << month << " Oct" << endl;
-            break;
-        case 11:
-            cout << i << " " << month << " Nov " << endl;
-            break;
-        case 12:
-            cout << i << " " << month << "Dec" << endl;
-        }
-
-}
-    }
-}
-
-int main() {
-    int month;
-    for (int i = 0; i < month; i++) {
-    cout << "Enter the Month";
-    cin >> month;
-}
-}
-
--- main-2-1.cpp
+++ main-2-1.cpp
@@ -1,10 +1,43 @@
-#include <iostream>
+
+
+#include<iostream>
 using namespace std;
 
-int main() {
-    int month;
-    for (int i = 0; i < month; i++) {
-    cout << "Enter the Month";
-    cin >> month;
+//array to store all months
+string arr[] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+
+//function to get month from index
+string lookup_month(int month){
+        
+        //variable to store month
+        string str = "";
+        
+        //if month is valid return from array
+        if(month >= 1 && month <= 12)
+                str = arr[month-1];
+        //otherwise set month as invalid        
+        else
+                str = "invalid month";
+        
+        //return month
+        return str;
+        
 }
+
+
+
+
+//test function
+int main(){
+        
+        //create a loop to call lookup_month repeatedly
+        for(int i=0;i<14;i++){
+                
+                //call lookup_month
+                cout << "calling lookup_month(" << i << ") =>    " << lookup_month(i) << endl;
+                
+        }
+        
+        return 0;
+        
 }
